8f8fe2d197798e0026c2f50d4adb3d28bd6e4e2a
nathan79-c <19kt219@esisalama.org>
2023-03-14 09:05:18 +0200

    creation du dossier Api et du controller PostController

--- database/migrations/2023_03_14_041852_create_posts_table.php
+++ database/migrations/2023_03_14_041852_create_posts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('titre');
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::get('test',function(){
+    return 'test';
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
