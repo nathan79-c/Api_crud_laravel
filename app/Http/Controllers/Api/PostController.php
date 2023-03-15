a69d5343ed5caba228a8040b8a93d84c51ba1e20
nathan79-c <19kt219@esisalama.org>
2023-03-15 02:52:06 +0200

    creation de la request createPostRequest

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'liste des articles';
+    }
+    public function store()
+    {
+        $Post = new Post();
+        $Post->titre = 'Titre exemple';
+        $Post->description = 'description exemple';
+        $Post->save();
+    }
 }
